daaa4e4ecc2764d228be6f00365c45395d739b54
lallanprasad1311@outlook.com <lallanprasad1311@outlook.com>
2024-02-25 16:42:36 +0530

    Add project files.

---
+++ comt34_agips/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace comt34_agips.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
